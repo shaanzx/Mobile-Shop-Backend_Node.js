3c4a6e3d7cf2a3a660bbdd9336e5d3833951a9e3
Shaanzx <anuhaslakshan07@gmail.com>
2025-06-28 16:36:01 +0530

    Create app.ts and change the structure

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express, {Express,Request,Response} from "express";
 // 01. Initialize the express app
 const app : Express = express();
 
+// 02. Define the Middlewares
+app.use(express.json());  /*Instruct to parse the request payload data to be converted to json*/
+
 // 03. Define simple http request
 app.get("/", (req : Request, res : Response) => {
     res.send("Request received...");
